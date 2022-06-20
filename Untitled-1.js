8a062239442e5c7ecaa4e731dd20912e86bea80e
unknown <lapinkolja@gmail.com>
2022-06-20 20:34:39 +0500

    Add first word on js

---
+++ Untitled-1.js
@@ -0,0 +1,41 @@
+let User_name = "Nik"
+let name2 = "Nik"
+
+console.log(name2)
+
+let age = 24
+console.log(age)
+
+let person = User_name + " " + age
+console.log(person)
+
+//console.log(name2 + "__" + age)
+
+let user_age =24
+let work_period =13
+
+let total_time = user_age - work_period
+console.log("total_time =" , total_time)
+
+let morning = false
+console.log ("morning =", morning, typeof(morning))
+
+let evening = true
+console.log("evening = ", evening,typeof(evening))
+
+let apples = 15
+let cherry = 2
+
+let compare = apples > cherry
+
+console.log("compare = ", compare)
+
+if (compare){
+    console.log("Copare is", compare)
+
+} else if (apples == 15){
+console.log ("Apples == ", apples)
+
+} else {
+        console.log("Copare is", compare)
+}
---
+++ len_2.js
@@ -0,0 +1,78 @@
+// console.log ("hello")
+// console.log(false || true)
+// console.log(true && false && true)
+// // && like *  || likr ather 
+// console.log(true && (false || true))
+// console.log(true && false || false && true)
+// console.log(true && false && true)
+ 
+// if (false || true){
+//     console.log("1")
+// } else {
+
+// console.log("2")
+// }
+
+// if (false || true || false){
+//     console.log("1")
+// } else {
+
+//     console.log("2")
+// }
+
+let age = 18
+let money = 1000
+let dog = true
+
+// if (age >= 18 && money >=1000 && dog){
+//     console.log("get ticket")
+// } else if (age <= 18){
+//     console.log("Early")
+// } else if (money  < 1000){
+//     console.log("Keep calm and work hard")
+// }else if(!dog){
+//     console.log("Get Cat")
+// }
+
+// if (age >= 18 && money >=1000 && dog){
+//     console.log("get ticket")
+// } else{         if (age <= 18){
+//                     console.log("Early")
+// }               if (money  < 1000){
+//                     console.log("Keep calm and work hard")
+// }               if(!dog){
+//                     console.log("Get Cat")
+// }}
+
+// switch(do_show) {
+//     case age >= 18:
+//         console.log ("Time to win")
+//     break
+    
+//     case money >= 1000:
+//         console.log("rich")
+        
+//     break
+// }
+
+// count = 0
+// while (count < 10 ){
+//     console.log(count, "win!!!")
+//     if (count % 2 == 0){
+//         console.log(count, "even")
+//     }
+//     count++
+// }
+
+// for (let i = 0; i < 1; i++){
+//     console.log(i, 'Win!!')
+
+// }
+
+mm=["a","b","c","d","e"]
+console.log(mm.length)
+
+for (let i = 0; i < mm.length ; ++i){
+    console.log(i,mm[i], 'Win!!')
+
+}
